266df9281ca1d7b1bd108953420451a008a65991
NaniSukmaP <luckyputri98@gmail.com>
2020-05-14 16:01:27 +0700

    initial commit

---
+++ Photomorgana/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Photomorgana
+//
+//  Created by Nani Sukma Putri Pratama on 14/05/20.
+//  Copyright © 2020 Nani Sukma Putri Pratama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //perubahan
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
