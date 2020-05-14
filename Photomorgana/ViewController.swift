382ffb6ffb2ecdf5ec7b639186f99e99904b0684
NaniSukmaP <luckyputri98@gmail.com>
2020-05-14 16:09:58 +0700

    penambahan comment

--- Photomorgana/ViewController.swift
+++ Photomorgana/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //perubahan
+        //ini perubahan ke dua
         // Do any additional setup after loading the view.
     }
 
